7342a5488d77840b9568d779bef5b7039e905df7
Manvi1807 <md7906@srmist.edu.in>
2026-03-03 09:32:50 +0530

    UC2: Display OOPS in banner format using println

---
+++ OOPSBannerApp(2).java
@@ -0,0 +1,15 @@
+public class OOPSBannerApp {
+
+    public static void main(String[] args) {
+
+        // Print OOPS in banner style
+
+        System.out.println("  *****   *****   *****   ***** ");
+        System.out.println(" *     * *     * *     * *     *");
+        System.out.println(" *     * *     * *     * *     *");
+        System.out.println(" *     * *     * *     * *     *");
+        System.out.println("  *****   *****   *****   ***** ");
+
+    }
+
+}
