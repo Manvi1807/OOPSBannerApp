4eda5335931d493dfe55612cef1818c862de8dfd
Manvi1807 <md7906@srmist.edu.in>
2026-03-03 10:23:21 +0530

    UC7: Added CharacterPattern class for banner mapping

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,6 +1,38 @@
-public class OOPSBannerApp{
- public static void main(String[] args{
-  System.out.println("OOPS");
-}
-}
+public class OOPSBannerApp {
 
+    public static void main(String[] args) {
+
+        CharacterPattern o = new CharacterPattern('O', new String[]{
+                " ***** ",
+                "*     *",
+                "*     *",
+                "*     *",
+                " ***** "
+        });
+
+        CharacterPattern p = new CharacterPattern('P', new String[]{
+                " ***** ",
+                "*     *",
+                " ***** ",
+                "*      ",
+                "*      "
+        });
+
+        CharacterPattern s = new CharacterPattern('S', new String[]{
+                " ***** ",
+                "*      ",
+                " ***** ",
+                "      *",
+                " ***** "
+        });
+
+        CharacterPattern[] patterns = { o, o, p, s };
+
+        for (int i = 0; i < 5; i++) {
+            for (CharacterPattern cp : patterns) {
+                System.out.print(cp.getPattern()[i] + "  ");
+            }
+            System.out.println();
+        }
+    }
+}
